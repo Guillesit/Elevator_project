d58a4eb2561824e43342288200cbcb17cf744923
Guillesit <100518581@alumnos.uc3m.es>
2024-09-16 20:52:52 +0200

    2 act

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import numpy as np
 
+Numb_floors=6
 Elevator_pos=0
 Current_target=0
 Current_direction="up"
 Max_speed=0
+Requests=np.zeros(Numb_floors)
+
+
+
+function Floor_index(Floor_num):
+    
+return Floor_num+lowest_floor
 
 
 
 
-
+Floor_index()
